c5483d2ff64f9263f3c3967c2b66b3f4beba6041
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-17 19:20:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+ 
+  get '/' do
+    "So there's no confusion."
+  end
   # This is a sample static route.
+ 
   get '/hello' do
     "Hello World!"
   end
@@ -14,5 +18,22 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+   get "/hello/:name" do
+    @given_name = params[:name]
+    "Hello #{@given_name}!"
+  end
 
+
+get '/goodbye/:name' do
+  @given_name = params[:name]
+  "Goodbye #{@given_name}."
+end
+
+get '/multiply/:num1/:num2' do
+  @num1 = params[:num1].to_i
+  @num2 = params[:num1].to_i
+  @answer = @num1 * @num2
+ @answer.to_s
+ erb :multiply
+ end
 end
